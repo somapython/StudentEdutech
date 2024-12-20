985ec7df67b679d9e2c3890b298847899e9a7b94
somapython <81561628+somapython@users.noreply.github.com>
2024-12-20 23:53:07 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-pink': '#e547cb', // your custom color
+      },
+    },
+  },
+  plugins: [],
+};
